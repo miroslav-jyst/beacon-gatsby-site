cfcce23991b5769642e6b349a0825917c687e7fa
Miroslav Lukic <ml@jystcreative.com>
2021-02-25 16:35:12 +0100

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     `gatsby-plugin-smoothscroll`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`
     {
       resolve: `gatsby-source-filesystem`,
       options: {
